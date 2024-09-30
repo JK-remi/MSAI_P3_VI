3a649841918c1dbf946df3fe4b21d3f7e9d0fc56
JK-remi <1abcm1@naver.com>
2024-09-30 11:40:29 +0900

    utils script add

---
+++ Assets/Resources/Scripts/Utils.cs
@@ -0,0 +1,49 @@
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using UnityEngine;
+
+public class Utils
+{
+    public const float AUDIO_REACTOR_FACTOR = 32767f; //to convert float to Int16
+
+    public const string SPEECH_REGION = "eastus";
+    public const string LANGUAGE = "ko-KR";
+
+    public static string GetFilePath(string filename)
+    {
+        string filpath = Path.Combine(Application.persistentDataPath, filename);
+        return filpath;
+    }
+
+    public static string SubJsonString(string target, string s, string e, int sOffset = 0, int eOffset = 0)
+    {
+        int startIdx = target.IndexOf(s);
+        if (startIdx < 0) return string.Empty;
+        int endIdx = target.IndexOf(e, startIdx);
+        if (endIdx < 0) return string.Empty;
+
+        startIdx = Mathf.Clamp(startIdx + sOffset, 0, target.Length);
+        endIdx = Mathf.Clamp(endIdx - eOffset, 0, target.Length);
+        string r = target.Substring(startIdx, endIdx - startIdx);
+        return r;
+    }
+
+    public static string RemoveSpecialChar(string str)
+    {
+        string result = Regex.Replace(str, @"[^0-9°¡-ÆR\s]", "");
+        return result;
+    }
+
+    public static string DecodeEncodedNonAsciiCharacters(string value)
+    {
+        return Regex.Replace(
+               value,
+               @"\\u(?<Value>[a-zA-Z0-9]{4})",
+               m =>
+               {
+                   return ((char)int.Parse(m.Groups["Value"].Value, NumberStyles.HexNumber)).ToString();
+               });
+    }
+
+}
