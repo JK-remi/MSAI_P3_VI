430766de34d28f6fb8d1d5b52bb1e97ef0f1d497
JK-remi <1abcm1@naver.com>
2024-10-04 09:10:08 +0900

    (script) GameManager update
    중복 클릭 방지 (isResponseEnd (true: 클릭 가능)
    paly audio add (TTS용)
    GPT/TTS Finish func add (GPT successs -> TTS)

--- Assets/Resources/Scripts/Managers/GameManager.cs
+++ Assets/Resources/Scripts/Managers/GameManager.cs
@@ -20,9 +20,11 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    [HideInInspector] private STT stt;
-    [HideInInspector] private TTS tts;
-    [HideInInspector] private GPT gpt;
+    private AudioSource audioSource;
+    private STT stt;
+    private TTS tts;
+    private GPT gpt;
+    private bool isResponseEnd = false;
 
     public YouTubeChat panelLiveChat;
 
@@ -33,16 +35,56 @@ public class GameManager : MonoBehaviour
 
     private void Init()
     {
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+
         stt = this.GetComponent<STT>();
         tts = this.GetComponent<TTS>();
         gpt = this.GetComponent<GPT>();
+
+        isResponseEnd = true;
     }
 
     public void Send2GPT(string prompt)
     {
+        if (isResponseEnd == false) return;
         if (panelLiveChat == null) return;
+
+        isResponseEnd = false;
+        Debug.Log("send to GPT");
 
         gpt.uiText = panelLiveChat.txtMessage;
         gpt.OnGPT(prompt);
+    }
+
+    public void PlayAudio(AudioClip clip)
+    {
+        if (clip == null) return;
+        if (audioSource.isPlaying && audioSource.clip == clip) return; // 현재 재생 중인 clip과 같으면 다시 재생 필요 X
+
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+
+    public void GptFinish(bool isSuccess)
+    {
+        if (isSuccess)
+        {
+            tts.StartTTS(gpt.uiText.text);
+        }
+        else
+        {
+            isResponseEnd = true;
+        }
+    }
+
+    public void TtsFinish(float wait)
+    {
+        if (isResponseEnd) return;
+        Invoke("ResponseEnd", wait);
+    }
+
+    public void ResponseEnd()
+    {
+        isResponseEnd = true;
     }
 }
