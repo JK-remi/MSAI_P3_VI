74e098d826adc476a08541abee14f6e0bb9ef807
JK-remi <1abcm1@naver.com>
2024-10-08 22:35:59 +0900

    (script) calibration data update
    프젝 성격에 맞춰서 수정
    - landmark transfrom 따로 갖고 있지 않아서 일단 eLandmark로 수정
    - init/cur direction (임시 - 진행하면서 수정)

--- Assets/Resources/Scripts/Mocap/CalibrationData.cs
+++ Assets/Resources/Scripts/Mocap/CalibrationData.cs
@@ -8,10 +8,11 @@ using UnityEngine;
 /// </summary>
 public class CalibrationData
 {
-    [SerializeField]
-    public string parentn, childn, tparentn, tchildn; // for doing a lookup at runtime
+    public string parentn, childn;
+    public eLandmark tparentn, tchildn; // for doing a lookup at runtime
     [System.NonSerialized]
-    public Transform parent, child, tparent, tchild;
+    public Transform parent, child;
+    public Vector3 vchild, vparent;
     [SerializeField]
     public Vector3 initialDir;
     [SerializeField]
@@ -25,29 +26,30 @@ public class CalibrationData
         parent.rotation = Quaternion.Lerp(parent.rotation, targetRotation, Time.deltaTime * speed);
     }
 
-    public Vector3 CurrentDirection => (tchild.position - tparent.position).normalized;
+    public Vector3 CurrentDirection => (vchild - vparent).normalized;
 
-    public CalibrationData(Transform topParent, Transform fparent, Transform fchild, Transform tparent, Transform tchild)
+    public CalibrationData(Transform fparent, Transform fchild, eLandmark tparent, eLandmark tchild)
     {
-        initialDir = (tchild.position - tparent.position).normalized;
         initialRotation = fparent.rotation;
 
         this.parent = fparent;
         this.child = fchild;
-        this.tparent = tparent;
-        this.tchild = tchild;
+        this.tparentn = tparent;
+        this.tchildn = tchild;
 
         parentn = GetPath(parent);
         childn = GetPath(child);
-        tparentn = GetPath(tparent);
-        tchildn = GetPath(tchild);
     }
+
+    public void SetInitDir(Vector3 parentPos, Vector3 childPos) 
+    {
+        initialDir = (childPos - parentPos).normalized;
+    }
+
     public CalibrationData ReconstructReferences()
     {
         SetFromPath(parentn, out parent);
         SetFromPath(childn, out child);
-        SetFromPath(tparentn, out tparent);
-        SetFromPath(tchildn, out tchild);
         return this;
     }
     private void SetFromPath(string path, out Transform target)
