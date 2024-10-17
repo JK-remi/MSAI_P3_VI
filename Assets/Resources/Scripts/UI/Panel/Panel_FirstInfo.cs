9338cd4ca69290f2cd47b037ee2b837cf7a3d90b
JK-remi <1abcm1@naver.com>
2024-10-17 15:35:50 +0900

    (script) UI Panel_FirstInfo Add
    처음 실행 시 안내 문구UI
    첫 실행 시만 나오고, 그 이후에는 안 나옴
    (확인 누르지 않은 상태에서 종료 시 다시 나올 수 있음)
    
    editor test용 코드 추가 (첫 실행 아니더라도 강제로 켜져있게 유지)
    
    prefab 적용 완료

---
+++ Assets/Resources/Scripts/UI/Panel/Panel_FirstInfo.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel_FirstInfo : PanelBase
+{
+    private const string PLAYER_PREF = "isFirst";
+
+#if UNITY_EDITOR
+    public bool bForceOpen = false;
+    private void Awake()
+    {
+        if (bForceOpen) PlayerPrefs.SetInt(PLAYER_PREF, 0);
+    }
+#endif
+
+    public bool isVisited = false;
+
+    protected override void Init()
+    {
+        if(isVisited || PlayerPrefs.GetInt(PLAYER_PREF, 0) > 0)
+        {
+            isVisited = true;
+
+            if(GameManager.Instance.curCharCnt > 0)
+            {
+                GameManager.Instance.OpenUI(ePanel.Modify);
+            }
+            else
+            {
+                GameManager.Instance.OpenUI(ePanel.Create);
+            }
+        }
+    }
+
+    public void OnOK()
+    {
+        isVisited = true;
+        PlayerPrefs.SetInt(PLAYER_PREF, 1);
+        PlayerPrefs.Save();
+        GameManager.Instance.OpenUI(ePanel.Create);
+    }
+}
