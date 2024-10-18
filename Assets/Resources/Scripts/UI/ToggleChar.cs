5cbaec6b2279eb840f88caa96a0d06a87d08cbb9
JK-remi <1abcm1@naver.com>
2024-10-18 16:55:41 +0900

    (script) toggle character add
    toggle(character select) 시 character info 전달 및 활성 (임시)

---
+++ Assets/Resources/Scripts/UI/ToggleChar.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleChar : MonoBehaviour
+{
+    public TextMeshProUGUI txtName;
+    public GameObject charObj;
+    private int charIdx;
+    private Toggle tgl;
+
+    private void Awake()
+    {
+        tgl = this.GetComponent<Toggle>();
+    }
+
+    public void Init(int idx)
+    {
+        charIdx = idx;
+
+        charObj = GameManager.Instance.GetCharObj(idx);
+        txtName.text = charObj.name;
+
+        tgl.onValueChanged.RemoveAllListeners();
+        tgl.onValueChanged.AddListener(new UnityEngine.Events.UnityAction<bool>(charObj.SetActive));
+        tgl.onValueChanged.AddListener(delegate { OnChange(); });
+    }
+
+    public void OnChange()
+    {
+        GameManager.Instance.curCharIdx = charIdx;
+    }
+
+    public void ToggleOn(bool isOn)
+    {
+        tgl.isOn = isOn;
+    }
+}
