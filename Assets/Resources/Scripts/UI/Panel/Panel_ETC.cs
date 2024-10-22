478d917684bb16be947741604ad6c7447d347c34
JK-remi <1abcm1@naver.com>
2024-10-22 22:59:28 +0900

    (script) panel etc add
    suggest 시 reset

---
+++ Assets/Resources/Scripts/UI/Panel/Panel_ETC.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Panel_ETC : PanelBase
+{
+    public TMP_InputField inputPros;
+    public TMP_InputField inputCons;
+    public TMP_InputField inputEmail;
+
+    protected override void Init()
+    {
+        inputPros.text = string.Empty;
+        inputCons.text = string.Empty;
+        inputEmail.text = string.Empty;
+    }
+
+    public override void ResetUI()
+    {
+        Init();
+    }
+
+    public void OnSummit()
+    {
+        GameManager.Instance.OpenNotice(eNotice.ETC_SUGGEST);
+    }
+}
