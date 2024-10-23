b39c2c6e410dfd8a5d0a78ad8b2d1ef1b2444ddc
MYSBS <qjatnekt567@gmail.com>
2024-10-23 16:29:57 +0900

    ppt_Prevent_AutoPlay
    자동재생 막음
    테스트video포함

--- Assets/Resources/Midia/MediaController.cs
+++ Assets/Resources/Midia/MediaController.cs
@@ -23,9 +23,13 @@ public class MediaController : MonoBehaviour
 
     private int mediaIndex = 0;             // 현재 미디어 인덱스
     private bool isSpecialImageActive = false;  // 특별 이미지 활성화 여부
+    private bool isVideoPlaying = false;    // 동영상 재생 중 여부
+    private bool isVideoPaused = false;     // 동영상 일시 정지 여부
 
     void Start()
     {
+        videoPlayer.playOnAwake = false;    // 자동 재생 비활성화
+        videoPlayer.loopPointReached += OnVideoFinished;
         specialImage.gameObject.SetActive(false); // 특별 이미지 비활성화
         ShowMedia();
     }
@@ -54,6 +58,39 @@ public class MediaController : MonoBehaviour
         ShowMedia();
     }
 
+    public void OnScreenClick()
+    {
+        if (isSpecialImageActive) return; // 특별 이미지가 표시 중이면 동작하지 않음
+
+        MediaItem currentItem = mediaItems[mediaIndex];
+
+        if (currentItem.mediaType == MediaItem.MediaType.Video)
+        {
+            if (!isVideoPlaying)
+            {
+                // 동영상 재생 시작
+                videoPlayer.Play();
+                isVideoPlaying = true;
+                isVideoPaused = false;
+            }
+            else
+            {
+                if (!isVideoPaused)
+                {
+                    // 동영상 일시 정지
+                    videoPlayer.Pause();
+                    isVideoPaused = true;
+                }
+                else
+                {
+                    // 동영상 다시 재생
+                    videoPlayer.Play();
+                    isVideoPaused = false;
+                }
+            }
+        }
+    }
+
     void ToggleSpecialImage()
     {
         isSpecialImageActive = !isSpecialImageActive;
@@ -67,6 +104,8 @@ public class MediaController : MonoBehaviour
             imageDisplay.gameObject.SetActive(false);
             videoDisplay.gameObject.SetActive(false);
             videoPlayer.Stop();
+            isVideoPlaying = false;
+            isVideoPaused = false;
         }
         else
         {
@@ -86,6 +125,8 @@ public class MediaController : MonoBehaviour
         imageDisplay.gameObject.SetActive(false);
         videoDisplay.gameObject.SetActive(false);
         videoPlayer.Stop();
+        isVideoPlaying = false;
+        isVideoPaused = false;
 
         MediaItem currentItem = mediaItems[mediaIndex];
 
@@ -97,10 +138,18 @@ public class MediaController : MonoBehaviour
         }
         else if (currentItem.mediaType == MediaItem.MediaType.Video)
         {
-            // 동영상 표시
+            // 동영상 표시 (자동 재생하지 않음)
             videoDisplay.gameObject.SetActive(true);
-            videoPlayer.clip = currentItem.video;
-            videoPlayer.Play();
+
+            videoPlayer.Stop();                  // 동영상 재생 중지
+            videoPlayer.clip = currentItem.video; // 동영상 클립 할당
+            // videoPlayer.Play();               // 자동 재생 코드 제거
         }
+    }
+
+    void OnVideoFinished(VideoPlayer vp)
+    {
+        isVideoPlaying = false;
+        isVideoPaused = false;
     }
 }
