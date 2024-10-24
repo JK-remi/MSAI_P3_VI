de4f661116839aeffb95608b76d5e76f21e006c4
JK-remi <1abcm1@naver.com>
2024-10-24 11:27:07 +0900

    (script) UI panel modify
    no use using del

--- Assets/Resources/Scripts/UI/Panel/Panel_Modify.cs
+++ Assets/Resources/Scripts/UI/Panel/Panel_Modify.cs
@@ -1,9 +1,6 @@
-using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class Panel_Modify : Panel_Create
